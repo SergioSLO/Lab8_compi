c79ead0af5743804d07192783b17e6ff750ed1c3
Sergio Lezama <98142685+SergioSLO@users.noreply.github.com>
2025-04-24 13:23:41 -0500

    Init

---
+++ cmake-build-debug/CMakeFiles/lab8.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/LENOVO/OneDrive/Escritorio/UTEC-ciclo-5/Compiladores/lab8/Lab8/exp.cpp" "CMakeFiles/lab8.dir/Lab8/exp.cpp.o" "gcc" "CMakeFiles/lab8.dir/Lab8/exp.cpp.o.d"
+  "/mnt/c/Users/LENOVO/OneDrive/Escritorio/UTEC-ciclo-5/Compiladores/lab8/Lab8/main.cpp" "CMakeFiles/lab8.dir/Lab8/main.cpp.o" "gcc" "CMakeFiles/lab8.dir/Lab8/main.cpp.o.d"
+  "/mnt/c/Users/LENOVO/OneDrive/Escritorio/UTEC-ciclo-5/Compiladores/lab8/Lab8/parser.cpp" "CMakeFiles/lab8.dir/Lab8/parser.cpp.o" "gcc" "CMakeFiles/lab8.dir/Lab8/parser.cpp.o.d"
+  "/mnt/c/Users/LENOVO/OneDrive/Escritorio/UTEC-ciclo-5/Compiladores/lab8/Lab8/scanner.cpp" "CMakeFiles/lab8.dir/Lab8/scanner.cpp.o" "gcc" "CMakeFiles/lab8.dir/Lab8/scanner.cpp.o.d"
+  "/mnt/c/Users/LENOVO/OneDrive/Escritorio/UTEC-ciclo-5/Compiladores/lab8/Lab8/token.cpp" "CMakeFiles/lab8.dir/Lab8/token.cpp.o" "gcc" "CMakeFiles/lab8.dir/Lab8/token.cpp.o.d"
+  "/mnt/c/Users/LENOVO/OneDrive/Escritorio/UTEC-ciclo-5/Compiladores/lab8/Lab8/visitor.cpp" "CMakeFiles/lab8.dir/Lab8/visitor.cpp.o" "gcc" "CMakeFiles/lab8.dir/Lab8/visitor.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab8.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab8.dir/Lab8/exp.cpp.o"
+  "CMakeFiles/lab8.dir/Lab8/exp.cpp.o.d"
+  "CMakeFiles/lab8.dir/Lab8/main.cpp.o"
+  "CMakeFiles/lab8.dir/Lab8/main.cpp.o.d"
+  "CMakeFiles/lab8.dir/Lab8/parser.cpp.o"
+  "CMakeFiles/lab8.dir/Lab8/parser.cpp.o.d"
+  "CMakeFiles/lab8.dir/Lab8/scanner.cpp.o"
+  "CMakeFiles/lab8.dir/Lab8/scanner.cpp.o.d"
+  "CMakeFiles/lab8.dir/Lab8/token.cpp.o"
+  "CMakeFiles/lab8.dir/Lab8/token.cpp.o.d"
+  "CMakeFiles/lab8.dir/Lab8/visitor.cpp.o"
+  "CMakeFiles/lab8.dir/Lab8/visitor.cpp.o.d"
+  "lab8"
+  "lab8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
